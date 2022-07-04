a50e53b7f7172eaea786a9e2f0d31e42639c8b92
Vishal Yadav <vishal.yadav@zemosolabs.com>
2022-07-04 09:36:33 +0530

    New endpoint to get a single topic

--- src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -7,13 +7,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 
-  private List<Topic> topics = Arrays.asList(
+  private final List<Topic> topics = Arrays.asList(
       new Topic("spring", "Spring Framework", "Spring Framework Description"),
       new Topic("java", "Core Java", "Core Java Description"),
-      new Topic("javascript", "JavaScript", "JavaScript Description")
-  );
+      new Topic("javascript", "JavaScript", "JavaScript Description"));
 
   public List<Topic> getAllTopics() {
     return topics;
+  }
+
+  public Topic getTopic(String id) {
+    return topics.stream().filter(topic -> topic.getId().equals(id)).findFirst().get();
   }
 }
