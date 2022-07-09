d2295889f85691d0f01d5285bb2bd5742f6096a4
Vishal Yadav <vishal.yadav@zemosolabs.com>
2022-07-09 13:25:55 +0530

    Adding update and delete methods with PUT and DELETE verbs

--- src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -24,4 +24,17 @@ public class TopicService {
   public void addTopic(Topic topic) {
     topics.add(topic);
   }
+
+  public void updateTopic(String id, Topic topic) {
+    for (int i = 0; i < topics.size(); i++) {
+      Topic t = topics.get(i);
+      if (t.getId().equals(id)) {
+        topics.set(i, topic);
+      }
+    }
+  }
+
+  public void deleteTopic(String id) {
+    topics.removeIf((topic -> topic.getId().equals(id)));
+  }
 }
